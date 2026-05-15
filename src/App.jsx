642fcfe763cc5f44e24cb68bccc31b111b77c12e
chetankumawat13 <kumawatchetan2025@gmail.com>
2026-05-15 23:00:50 +0530

    feat: implement edit functionality for notes in the app

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ const App = () => {
      const data = localStorage.getItem('formData') 
      return data ? JSON.parse(data) : []
   })
+  const [editData, setEditData] = useState(null)
 
   useEffect(() => {
     localStorage.setItem('formData',JSON.stringify(formData))
@@ -19,13 +20,20 @@ const App = () => {
       setFormData(prev => prev.filter((_,i) => i !== index ))
   }
 
+  const handleEdit = (index) => {
+     setEditData({
+        ...formData[index],
+        index 
+     })
+  }
+
 
   return (
     <div className='main'>
-      <Form formData={formData}  setFormData={setFormData} />
+      <Form formData={formData}  setFormData={setFormData} editData={editData} setEditData={setEditData} />
       <h2>Notes</h2>
       <div className="cards">
-        <Card formData={formData} handleDelete={handleDelete} />
+        <Card handleEdit={handleEdit} formData={formData} handleDelete={handleDelete} />
       </div>
     </div>
   )
